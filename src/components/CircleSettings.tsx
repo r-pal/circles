b2208b6fbdda0858355c986d6d15b22bc2a60b09
dazzle-tactics <90906037+dazzle-tactics@users.noreply.github.com>
2023-03-09 10:39:28 +0000

    init headless ui and colour paletter

--- src/components/CircleSettings.tsx
+++ src/components/CircleSettings.tsx
@@ -1,5 +1,7 @@
 import { useForm, SubmitHandler } from "react-hook-form";
 import { Colour } from "./Types";
+import { useState } from 'react'
+import { Listbox } from '@headlessui/react'
 
 type CircleSettingsProps = {};
 
@@ -9,6 +11,7 @@ type Inputs = {
 };
 
 const CircleSettings: React.FC<CircleSettingsProps> = ({}) => {
+
   const {
     register,
     handleSubmit,
@@ -17,16 +20,32 @@ const CircleSettings: React.FC<CircleSettingsProps> = ({}) => {
   } = useForm<Inputs>();
   const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
 
+  const colours = [
+    {id: 1, name: "Dark purple", hex:"#3A3042"},
+    {id: 2, name: "Earth yellow", hex:"#DB9D47"},
+    {id: 3, name: "Coral", hex:"#FF784F"},
+    {id: 4, name: "Peach Yellow", hex:"#FFE19C"},
+    {id: 5, name: "Nyanza", hex:"#EDFFD9"}]
+
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <>
         <div className="sm:grid sm:grid-cols-2 sm:gap-8">
+          Radius
           <input
             required
             id="radius"
             placeholder="Enter radius in cm"
             {...register("radius")}
           />
+          Colour
+          <input
+            required
+            id="radius"
+            placeholder="Choose colour"
+            {...register("radius")}
+          />
+          <button type="submit">Draw Circle</button>
         </div>
       </>
     </form>
