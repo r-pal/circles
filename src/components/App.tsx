af63360df4e39c4bd69b7a6719f52d6f79c48963
r-pal <90906037+r-pal@users.noreply.github.com>
2023-03-14 12:11:13 +0000

    settings sidebar

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,76 @@
-import CircleSettings from "./CircleSettings";
+import { Faders } from "phosphor-react";
+import { useState, useEffect } from "react";
+import Button from "./Button";
+import CircleSettings, { Inputs } from "./CircleSettings";
 import Footer from "./Footer";
-import Header from "./Header";
+import Sketch from "./Sketch";
 
 const App: React.FC = () => {
+  const [gameResult, setGameResult] = useState<"won" | "lost">();
+  const [wins, setWins] = useState(0);
+  const [circleSketch, setCircleSketch] = useState<Inputs>();
+  const [jiggliness, setJiggliness] = useState(0);
+
+  useEffect(() => {
+    if (gameResult === "won") {
+      setWins(wins + 1);
+    }
+    if (wins === 3) {
+      //enter winner board
+    }
+  }, [gameResult]);
   return (
     <div>
-      <Header />
-      <CircleSettings />
-      <Footer />
+      <div className="drawer">
+        <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+        <div className="drawer-content">
+          <div className="bg-[#3A3042] h-[156px] flex justify-between">
+            <h1 className="text-[#EDFFD9] text-3xl text-center ">CIRCLES</h1>
+            <div className="flex flex-col gap-4 w-full text-center pt-4">
+              {gameResult ? (
+                <button
+                  form="settings"
+                  onClick={() => setGameResult(undefined)}
+                >
+                  <Button text="Play again" />
+                </button>
+              ) : (
+                <button form="settings" type="submit">
+                  <Button text="Start game" />
+                </button>
+              )}
+            </div>
+            <label
+              htmlFor="my-drawer"
+              className="btn rounded-full bg-[#EDFFD9]"
+            >
+              <Faders size={32} className="text-[#3A3042]" />
+            </label>
+          </div>
+          <div className="bg-[#315964]">
+            {circleSketch && gameResult === undefined && (
+              <Sketch
+                circleSketch={circleSketch}
+                jiggliness={jiggliness}
+                setGameResult={setGameResult}
+              />
+            )}
+            {gameResult === "won" && (
+              <h1 className="text-[#EDFFD9] text-9xl text-center ">WINNER</h1>
+            )}
+            {gameResult === "lost" && (
+              <h1 className="text-[#EDFFD9] text-9xl text-center ">YOU LOSE</h1>
+            )}
+          </div>
+          <Footer />
+        </div>
+        <div className="drawer-side">
+          <label htmlFor="my-drawer" className="drawer-overlay"></label>
+          <ul className="menu p-4 w-80 bg-[#DB9D47]">
+            <CircleSettings setCircleSketch={setCircleSketch} />
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/CircleSettings.tsx
+++ src/components/CircleSettings.tsx
@@ -1,137 +1,96 @@
 import { useForm, SubmitHandler } from "react-hook-form";
 import { Colour } from "./Types";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { colours } from "../constants/colours";
-import Button from "./Button";
-import Sketch from "./Sketch";
 import clsx from "clsx";
-
-type CircleSettingsProps = {};
 
 export type Inputs = {
   radius: number;
   instances: number;
   colour: Colour;
+  jiggliness: number;
 };
 
-const CircleSettings: React.FC<CircleSettingsProps> = () => {
-  const [gameResult, setGameResult] = useState<"won" | "lost">();
-  const [wins, setWins] = useState(0);
-  const [circleSketch, setCircleSketch] = useState<Inputs>();
+type CircleSettingsProps = {
+  setCircleSketch: (value: Inputs) => void;
+};
+
+const CircleSettings: React.FC<CircleSettingsProps> = ({ setCircleSketch }) => {
   const [selectedColour, setSelectedColour] = useState("");
-  const [jiggliness, setJiggliness] = useState(0);
   const { register, handleSubmit, control } = useForm<Inputs>();
   const onSubmit: SubmitHandler<Inputs> = (data) => {
     setCircleSketch(data);
-    console.log(data);
   };
-  // let wins = 0;
-
-  useEffect(() => {
-    if (gameResult === "won") {
-      setWins(wins + 1);
-    }
-    if (wins === 3) {
-      //enter winner board
-    }
-  }, [gameResult]);
-  console.log("wins: ", wins);
 
   return (
     <div>
-      <div className="bg-[#DB9D47] grid content-center h-[156px]">
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <div className="w-full bg-[#DB9D47] flex flex-col items-center gap-3">
-            <div>
-              Size
-              <input
-                type="range"
-                step="50"
-                min="10"
-                max="1010"
-                className="range range-md border-[#EDFFD9]"
-                id="size"
-                defaultValue={50}
-                {...register("radius")}
-              />
-            </div>
-            <div>
-              Number
-              <input
-                type="range"
-                step="1"
-                min="1"
-                max="100"
-                className="range range-md bg-[#EDFFD9]"
-                id="instances"
-                defaultValue={1}
-                {...register("instances")}
-              />
-            </div>
-            <div>
-              Colour
-              <select
-                {...register("colour")}
-                className={clsx(
-                  "selectedColour && bg-[${selectedColour}]",
-                  "selectedColour.id === 1 && text-bg-[#EDFFD9]"
-                )}
-                onChange={(e) => setSelectedColour(e.target.value)}
-              >
-                {colours.map((c) => (
-                  <option
-                    key={c.id}
-                    value={c.hex}
-                    className={clsx(`bg-[${c.hex}]`)}
-                  >
-                    {c.name}
-                  </option>
-                ))}
-              </select>
-            </div>
-            <div>
-              Jiggly Factor
-              <input
-                type="range"
-                step="1"
-                min="1"
-                max="10"
-                value={jiggliness}
-                className="range range-md"
-                id="Jigglyness"
-                onChange={(e) => setJiggliness(Number(e.target.value))}
-              />
-            </div>
+      {/* <div className="bg-[#DB9D47] grid content-center"> */}
+      <form id="settings" onSubmit={handleSubmit(onSubmit)}>
+        <div className="w-full bg-[#DB9D47] flex flex-col items-left gap-3">
+          <div>
+            Size
+            <input
+              type="range"
+              step="50"
+              min="10"
+              max="1010"
+              className="range range-md border-[#EDFFD9]"
+              id="size"
+              defaultValue={50}
+              {...register("radius")}
+            />
           </div>
-          <div className="flex flex-col gap-4 w-full text-center pt-4">
-            {gameResult ? (
-              <button onClick={() => setGameResult(undefined)}>
-                <Button text="Reset" />
-              </button>
-            ) : (
-              <button type="submit">
-                <Button text="Start game" />
-              </button>
-            )}
+          <div>
+            Number
+            <input
+              type="range"
+              step="1"
+              min="1"
+              max="100"
+              className="range range-md"
+              id="instances"
+              defaultValue={1}
+              {...register("instances")}
+            />
           </div>
-        </form>
-      </div>
-      <div className="bg-[#315964]">
-        {circleSketch && gameResult === undefined && (
-          <Sketch
-            circleSketch={circleSketch}
-            jiggliness={jiggliness}
-            setGameResult={setGameResult}
-          />
-        )}
-        {gameResult === "won" && (
-          <h1 className="text-[#EDFFD9] text-9xl text-center ">WINNER</h1>
-        )}
-        {gameResult === "lost" && (
-          <h1 className="text-[#EDFFD9] text-9xl text-center ">YOU LOSE</h1>
-        )}
-      </div>
+          <div className="flex-col">
+            <div>Colour</div>
+            <select
+              {...register("colour")}
+              className={clsx(
+                "selectedColour && bg-[${selectedColour}]",
+                "selectedColour.id === 1 && text-bg-[#EDFFD9]"
+              )}
+              onChange={(e) => setSelectedColour(e.target.value)}
+            >
+              {colours.map((c) => (
+                <option
+                  key={c.id}
+                  value={c.hex}
+                  className={clsx(`bg-[${c.hex}]`)}
+                >
+                  {c.name}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div>
+            Jiggly Factor
+            <input
+              type="range"
+              step="1"
+              min="1"
+              max="10"
+              defaultValue={1}
+              className="range range-md"
+              id="jigglyness"
+              {...register("jiggliness")}
+            />
+          </div>
+        </div>
+      </form>
     </div>
+    // </div>
   );
 };
 
